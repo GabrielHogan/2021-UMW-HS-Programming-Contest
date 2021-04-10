34f4f4f0057efebc34f569ba3f46405743ec4bf5
GabrielHogan <gabriel.t.hogan@gmail.com>
2021-04-10 16:03:08 -0400

    Add ProblemD Solution

---
+++ ProblemD.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int el = scan.nextInt() / 10;
+        int sc = scan.nextInt() / 4;
+
+
+        if(el<=sc)
+        System.out.println( el * 10);
+
+        if(sc<el)
+        System.out.println( sc * 10);
+    }
+}
